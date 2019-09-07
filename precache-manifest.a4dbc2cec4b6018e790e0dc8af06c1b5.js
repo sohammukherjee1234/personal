a55edcd3125a25cad3c69c460e9420591691ea65
sohammukherjee1234 <rockstarsam1234@gmail.com>
2019-09-08 02:36:56 +0530

    Updates

---
+++ precache-manifest.a4dbc2cec4b6018e790e0dc8af06c1b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d01a097f9f347c4c1e979bcba26eac",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "02612f880e0dde3d3ec2",
+    "url": "/personal/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "86a1ffd71ac98b2a163d",
+    "url": "/personal/static/js/2.197f4c2c.chunk.js"
+  },
+  {
+    "revision": "02612f880e0dde3d3ec2",
+    "url": "/personal/static/js/main.6b845300.chunk.js"
+  },
+  {
+    "revision": "aaa02586280be43056ed",
+    "url": "/personal/static/js/runtime~main.686f438b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal/static/media/logo.5d5d9eef.svg"
+  }
+]);
